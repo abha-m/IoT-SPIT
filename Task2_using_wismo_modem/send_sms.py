355f4437fe38aa0faf68b7830131b40673a23d7e
SaiArrow <saireddy02091997@gmail.com>
2017-07-27 20:40:42 +0530

    Create send_sms.py
    Demo Python Script to Send Messages via List 
    Yet to be integrated in Main Script

---
+++ Task2_using_wismo_modem/send_sms.py
@@ -0,0 +1,35 @@
+import serial
+import time
+import sys
+
+ser = serial.Serial(port='/dev/ttyS0',baudrate=115200,timeout=2)
+print ser
+ser.write("AT\r")
+print ser.read(20)
+ser.write("AT+CSQ\r")
+print ser.read(20)
+ser.write('AT+CMGF=1\r')
+print ser.read(20)
+
+myString = "Hello"
+myList = ["8767658591","7738945436","9920067697"]
+
+message = myString
+for i in range(len(myList)):
+    phoneNumber = myList[i]   
+    print "number = " + phoneNumber + ", message = " + message
+    time.sleep(5)
+    ser.write('''AT+CMGS="''' + phoneNumber + '''"\r''')
+    print ser.read(20)
+    time.sleep(5)
+    ser.write(message + "\r")
+    time.sleep(5)
+    ser.write(chr(26))
+
+
+#Main function that calls other functions - Makes script reusable
+def main():
+	pass
+
+	if __name__ == "__main__":
+		main()
